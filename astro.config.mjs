f4f50a68967e0e912bd45cde8bf840c66bf9182f
Josh <joshua.sankarlal@gmail.com>
2026-06-08 17:57:32 -0400

    Init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts:[{
+        provider: fontProviders.fontsource(),
+        name:"Public Sans",
+        cssVariable: "--font-public",
+    },
+    {
+        provider: fontProviders.fontsource(),
+        name: "Source Sans 3",
+        cssVariable: "--source-sans-3",
+    },
+]
+
+});
